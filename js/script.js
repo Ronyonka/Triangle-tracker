288a8f663bfe5f88c66ab6292df4bc5eac7140f0
Ronyonka <rononyonka@gmail.com>
2018-12-01 11:16:21 +0300

    added formula

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
-var a = getElementById('avalue');
-var b = getElementById('bvalue');
-var c = getElementById('cvalue');
+var a = getElementById('avalue').submit();
+var b = getElementById('bvalue').submit();
+var c = getElementById('cvalue').submit();
+
+
+
+if (a+b>c && b+c>a && c+a>b){
+  if(a == b && b ==c){
+    alert("Triangle is Equilateral");
+  }else if (a == b && b !== c ){
+    alert("Triangle is iscoceles");
+  }else if (c == b && a !== c ){
+    alert("Triangle is iscoceles");
+  }else if (a == c && b !== c ){
+    alert("Triangle is iscoceles");
+  }else if (a !== b && b !== c && a !== c){
+    alert("Triangle is Scalene");
+  }
+}else {
+alert("This is not a triangle");
+}
