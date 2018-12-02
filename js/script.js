3dfe66f22dfde4b02db4b4b3988c0a26afb63d5a
Ronyonka <rononyonka@gmail.com>
2018-12-02 23:56:12 +0300

    changed button into input

--- js/script.js
+++ js/script.js
@@ -1,24 +1,19 @@
-// var a = getElementById('avalue');
-// var b = getElementById('bvalue');
-// var c = getElementById('cvalue');
-var triangle = ['getElementById('avalue')', 'getElementById('bvalue')', 'getElementById('cvalue')']
+var a = parseInt(getElementById('avalue')).value;
+var b = parseInt(getElementById('bvalue')).value;
+var c = parseInt(getElementById('cvalue')).value;
 
-triangle.forEach(function(triangle)){
-  if
-}
-
-// function(checkTriangle){if (a+b>c && b+c>a && c+a>b){
-//     if(a == b && b ==c){
-//       alert("Triangle is Equilateral");
-//     }else if (a == b && b !== c ){
-//       alert("Triangle is iscoceles");
-//     }else if (c == b && a !== c ){
-//       alert("Triangle is iscoceles");
-//     }else if (a == c && b !== c ){
-//       alert("Triangle is iscoceles");
-//     }else if (a !== b && b !== c && a !== c){
-//       alert("Triangle is Scalene");
-//     }
-//   }else {
-//   alert("This is not a triangle");
-// }}
+getElementById('submit').onClick = function(){if (a+b>c && b+c>a && c+a>b){
+      if(a == b && b ==c){
+        alert("Triangle is Equilateral");
+      }else if (a == b && b !== c ){
+        alert("Triangle is iscoceles");
+      }else if (c == b && a !== c ){
+        alert("Triangle is iscoceles");
+      }else if (a == c && b !== c ){
+        alert("Triangle is iscoceles");
+      }else if (a !== b && b !== c && a !== c){
+        alert("Triangle is Scalene");
+      }
+    }else {
+    alert("This is not a triangle");
+  }}
