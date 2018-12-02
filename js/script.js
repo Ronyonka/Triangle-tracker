2438a8583dea8a5f7d1087ec3303df88598d2181
Ronyonka <rononyonka@gmail.com>
2018-12-03 00:08:11 +0300

    added functions to js file

--- js/script.js
+++ js/script.js
@@ -2,18 +2,17 @@ var a = parseInt(getElementById('avalue')).value;
 var b = parseInt(getElementById('bvalue')).value;
 var c = parseInt(getElementById('cvalue')).value;
 
-getElementById('submit').onClick = function(){if (a+b>c && b+c>a && c+a>b){
-      if(a == b && b ==c){
-        alert("Triangle is Equilateral");
-      }else if (a == b && b !== c ){
-        alert("Triangle is iscoceles");
-      }else if (c == b && a !== c ){
-        alert("Triangle is iscoceles");
-      }else if (a == c && b !== c ){
-        alert("Triangle is iscoceles");
-      }else if (a !== b && b !== c && a !== c){
-        alert("Triangle is Scalene");
-      }
-    }else {
+var triangle = function(a, b, c){if (a+b>c && b+c>a && c+a>b){
+    return triangle;
+  }else {
     alert("This is not a triangle");
+  }}
+
+
+var checkTriangle = function(triangle){if(a == b && b ==c){
+    alert("Triangle is Equilateral");
+  }else if (a == b || b == c || a == c ){
+    alert("Triangle is iscoceles");
+  }else if (a !== b && b !== c && a !== c){
+    alert("Triangle is Scalene");
   }}
