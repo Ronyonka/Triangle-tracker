9216c7f8f976edc1fbac01451ac18abf0f6b74a3
Ronyonka <rononyonka@gmail.com>
2018-12-03 01:23:00 +0300

    added query for results

--- js/script.js
+++ js/script.js
@@ -1,17 +1,25 @@
 function triangleTracker(){
-var a = document.getElementById('avalue').value();
-var b = document.getElementById('bvalue').value();
-var c = document.getElementById('cvalue').value();
+var a = parseInt(document.getElementById('avalue').value());
+var b = parseInt(document.getElementById('bvalue').value());
+var c = parseInt(document.getElementById('cvalue').value());
 
-var triangle = function(a, b, c){if (a+b>c && b+c>a && c+a>b){
+var triangle = [];
+
+triangle.push("a");
+triangle.push("b");
+triangle.push("c");
+
+var results = document.querySelector("#results");
+
+if (a+b>c && b+c>a && c+a>b){
   if(a == b && b ==c){
-    alert("Triangle is Equilateral");
+    results.innerHTML="Your Triangle is Equilateral";
   }else if (a == b || b == c || a == c ){
-    alert("Triangle is iscoceles");
+    results.innerHTML="Triangle is iscoceles";
   }else{
-    alert("Triangle is Scalene");
+    results.innerHTML="Triangle is Scalene";
   }
 }
   }else {
-    alert("This is not a triangle");
+    results.innerHTML="This is not a triangle";
   }}
