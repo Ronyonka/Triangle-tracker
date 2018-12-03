93d281c6fae3f46705f0b54f88aa80c20bbb711d
Ronyonka <rononyonka@gmail.com>
2018-12-03 03:38:59 +0300

    added an if nan function

--- js/script.js
+++ js/script.js
@@ -3,15 +3,17 @@ function triangleTracker(){
   var b = parseInt(document.getElementById('bvalue').value);
   var c = parseInt(document.getElementById('cvalue').value);
 
-  if(a+b>c && b+c>a && c+a>b){
-    if(a == b && b == c){
-      alert("Your Triangle is Equilateral");
-    }else if (a == b || b == c || a == c){
-      alert("Your Triangle is Isoceles");
+  if(isNaN(a) || isNaN(b) || isNaN(c)){
+      alert("Seems like you never got \"Round\" to selecting all the dimensions...Tri-again");
+    }else if(a+b>c && b+c>a && c+a>b){
+      if(a == b && b == c){
+        alert("Your Triangle is Equilateral");
+      }else if (a == b || b == c || a == c){
+        alert("Your Triangle is Isoceles");
+      }else{
+        alert("You have discovered a scalene triangle");
+      }
     }else{
-      alert("You have discovered a scalene triangle");
+      alert("Your dimensions do not form a triangle. Tri-again...");
     }
-  }else{
-    alert("This is a bermuda triangle i.e it is none existent");
   }
-}
