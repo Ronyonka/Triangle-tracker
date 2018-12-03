8bdbb3775e18d9013a9a0dc190ff6aac055a6b98
Ronyonka <rononyonka@gmail.com>
2018-12-03 03:49:25 +0300

    added the query element and styled the h2

--- js/script.js
+++ js/script.js
@@ -3,17 +3,19 @@ function triangleTracker(){
   var b = parseInt(document.getElementById('bvalue').value);
   var c = parseInt(document.getElementById('cvalue').value);
 
+  var results = document.querySelector("#results");
+
   if(isNaN(a) || isNaN(b) || isNaN(c)){
-      alert("Seems like you never got \"Round\" to selecting all the dimensions...Tri-again");
+       results.innerHTML="Seems like you never got \"Round\" to selecting all the dimensions...Tri-again";
     }else if(a+b>c && b+c>a && c+a>b){
       if(a == b && b == c){
-        alert("Your Triangle is Equilateral");
+         results.innerHTML="Your Triangle is Equilateral";
       }else if (a == b || b == c || a == c){
-        alert("Your Triangle is Isoceles");
+         results.innerHTML="Your Triangle is Isoceles";
       }else{
-        alert("You have discovered a scalene triangle");
+         results.innerHTML="You have discovered a scalene triangle";
       }
     }else{
-      alert("Your dimensions do not form a triangle. Tri-again...");
+       results.innerHTML="Your dimensions do not form a triangle. Tri-again...";
     }
   }
